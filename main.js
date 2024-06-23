76265046b39fd3b1d954abfe5cb8e3b7f43e4a15
kkeevviinn123 <monteirokevin735@gmail.com>
2024-06-23 18:51:22 -0300

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-function showPopup() {
-    document.getElementById("popup-overlay").style.display = "block";
-    document.getElementById("popup").style.display = "block";
-}
-
-function hidePopup() {
-    document.getElementById("popup-overlay").style.display = "none";
-    document.getElementById("popup").style.display = "none";
-}
