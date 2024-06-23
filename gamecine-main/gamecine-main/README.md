842b5139ebfda0885ef3d9881ad204a44a6499ad
kkeevviinn123 <monteirokevin735@gmail.com>
2024-06-23 19:34:32 -0300

    Pastas atualizadas

---
+++ gamecine-main/gamecine-main/README.md
@@ -0,0 +1,1 @@
+This is a clone of Netflix initial page using HTML and CSS.
---
+++ projetoIntegrador-main/main.js
@@ -0,0 +1,19 @@
+function showPopup() {
+    document.getElementById("popup-overlay").style.display = "block";
+    document.getElementById("popup").style.display = "block";
+}
+
+function hidePopup() {
+    document.getElementById("popup-overlay").style.display = "none";
+    document.getElementById("popup").style.display = "none";
+}
+
+function showLoginPopup() {
+    document.getElementById("popup-overlay").style.display = "block";
+    document.getElementById("login-popup").style.display = "block";
+}
+
+function hideLoginPopup() {
+    document.getElementById("popup-overlay").style.display = "none";
+    document.getElementById("login-popup").style.display = "none";
+}
